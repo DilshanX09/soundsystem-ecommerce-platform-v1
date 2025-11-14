b297f7d45aaff4b322401cd95e3090b4ce4ec1a7
Chamod Dilshan <chamoddilshan834@gmail.com>
2025-11-14 16:12:59 +0530

    Add loading overlay and enhance account verification process with API integration

--- src/views/AccountVerification.view.tsx
+++ src/views/AccountVerification.view.tsx
@@ -1,7 +1,9 @@
 import OTPInput from 'react-otp-input';
 import Logo from '../assets/images/Dark-Logo.jpg';
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import apiService from '../services/apiService';
+import LoadingView from './Loading.view';
 
 interface LocationState {
      email: string;
@@ -9,14 +11,61 @@ interface LocationState {
 
 const AccountVerificationView = () => {
 
-     const [code, setCode] = useState("");
+     const [code, setCode] = useState<string>("");
      const handleChange = (code: string) => setCode(code);
+     const [responseMessage, setResponseMessage] = useState<string | null>(null);
+     const [isError, setIsError] = useState<boolean>(false);
+
+     const [isLoading, setIsLoading] = useState<boolean>(false);
 
      const location = useLocation();
      const { email } = (location.state as LocationState) || {};
 
+     const navigator = useNavigate();
+
+     const verificationProcessHandle = async () => {
+
+          setIsLoading(true);
+
+          if (code.length !== 6) {
+               setResponseMessage("Please enter a valid 6-digit code.");
+               setIsError(true);
+               setIsLoading(false);
+               return;
+          }
+
+          setIsError(false);
+
+          await apiService.post('/users-verification/verify-account', {
+               email: email,
+               verificationCode: code
+          })
+               .then(response => {
+                    if (response.data.status) {
+                         setIsLoading(false);
+                         setResponseMessage(response.data.message);
+                         setIsError(false);
+                         navigator('/users/authenticate');
+                         return;
+                    } else {
+                         setIsLoading(false);
+                         setResponseMessage(response.data.message);
+                         setIsError(true);
+                         return;
+                    }
+               })
+               .catch(() => {
+                    setIsLoading(false);
+                    setResponseMessage("An error occurred while processing your request. Please try again later.");
+                    setIsError(true);
+
+               });
+     }
+
      return (
           <div className="flex items-center justify-center pt-28">
+
+               {isLoading && <LoadingView />}
 
                <div className='w-[500px]'>
                     <div className="mb-4">
@@ -31,9 +80,13 @@ const AccountVerificationView = () => {
                          Please verify your account to unlock member benefits, track your orders, and discover premium audio equipment tailored for you.
                     </p>
 
-                    <p className="text-gray-500 mb-8 font-inter-regular text-base md:text-md">
+                    <p className="text-gray-500 mb-6 font-inter-regular text-base md:text-md">
                          We've sent a verification code to: <span className='font-inter-medium text-black underline'>{email}</span>
                     </p>
+
+                    <div className="flex flex-col">
+                         {responseMessage && <p className="text-[14px] font-inter-regular mb-3 text-red-500">{responseMessage}</p>}
+                    </div>
 
                     <OTPInput
                          value={code}
@@ -43,15 +96,15 @@ const AccountVerificationView = () => {
                          renderSeparator={<span></span>}
                          shouldAutoFocus={true}
                          inputStyle={{
-                              border: "1px solid #e4e4e4",
+                              border: isError ? "0.50px solid oklch(63.7% 0.237 25.331)" : "0.50px solid #e4e4e4",
                               fontFamily: 'inter-regular',
                               width: "54px",
                               height: "54px",
                               fontSize: "23px",
-                              color: "#000",
-                              borderRadius: '8px',
+                              color: "#000000",
+                              borderRadius: '5px',
                               fontWeight: "400",
-                              caretColor: "blue",
+                              caretColor: "black",
                               fontSizeAdjust: "20px",
                               marginRight: "10px",
                               outline: 'none'
@@ -59,6 +112,7 @@ const AccountVerificationView = () => {
                     />
 
                     <button
+                         onClick={verificationProcessHandle}
                          className="px-8 mt-5 bg-[#181818] text-white font-inter-medium text-[14px] py-3 rounded-[7px] hover:bg-[#2a2a2a] transition-colors mb-6"
                     >
                          SUBMIT
